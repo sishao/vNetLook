879061e21b48ca3d08634b78745b74c4085e2d36
simpsons.liux <simpsons.liux@alibaba-inc.com>
2013-06-27 11:26:08 +0800

    Check for apps which has network permissions only

--- vNetLook/src/main/java/org/vliux/netlook/LoadNetUseAsyncTask.java
+++ vNetLook/src/main/java/org/vliux/netlook/LoadNetUseAsyncTask.java
@@ -6,6 +6,7 @@ import android.net.TrafficStats;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 
 import org.vliux.netlook.model.AppNetUse;
 import org.vliux.netlook.model.TotalNetUse;
@@ -13,9 +14,14 @@ import org.vliux.netlook.model.TotalNetUse;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class LoadNetUseAsyncTask extends AsyncTask<Void, Integer, TotalNetUse> {
+    private static final String TAG = "LoadNetUseAsyncTask";
     public static final int MSG_COMPLETED = 1;
+    public static final String[] NETWORK_PERMISSIONS = new String[]{
+        "android.permission.INTERNET"
+    };
 
     private PackageManager mPackageManager;
     private Handler mHandler;
@@ -25,19 +31,40 @@ public class LoadNetUseAsyncTask extends AsyncTask<Void, Integer, TotalNetUse> {
         mHandler = handler;
     }
 
+    private boolean hasNetworkPermission(PackageInfo pkgInfo){
+        if(null == pkgInfo || null == pkgInfo.requestedPermissions){
+            return false;
+        }
+
+        for(String perm : pkgInfo.requestedPermissions){
+            for(String p : NETWORK_PERMISSIONS){
+                if(p.equals(perm)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     @Override
     protected TotalNetUse doInBackground(Void... voids) {
         TotalNetUse totalUse = new TotalNetUse();
         if(null == mPackageManager){
             return totalUse;
         }
-        List<PackageInfo> packages = mPackageManager.getInstalledPackages(PackageManager.GET_META_DATA);
+        List<PackageInfo> packages = mPackageManager.getInstalledPackages(PackageManager.GET_META_DATA | PackageManager.GET_PERMISSIONS);
         for(PackageInfo pkg : packages){
-            long rxBytes = TrafficStats.getUidRxBytes(pkg.applicationInfo.uid);
-            long txBytes = TrafficStats.getUidTxBytes(pkg.applicationInfo.uid);
-            if(rxBytes + txBytes <= 0L){
+            if(!hasNetworkPermission(pkg)){
+                Log.w(TAG, String.format(Locale.US, "Package %s doesn't have network permission", pkg.packageName));
                 continue;
             }
+
+            long rxBytes = TrafficStats.getUidRxBytes(pkg.applicationInfo.uid);
+            long txBytes = TrafficStats.getUidTxBytes(pkg.applicationInfo.uid);
+            //if(rxBytes + txBytes <= 0L){
+            //    Log.w(TAG, String.format(Locale.US, "Package %s ", pkg.packageName));
+            //    continue;
+            //}
 
             AppNetUse appUse = new AppNetUse();
             appUse.setmPackageName(pkg.packageName);
